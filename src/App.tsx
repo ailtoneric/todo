74fd33bebaa67913044fed89dead193ed73433be
Ailton Oliveira <sr.ailtoneric@gmail.com>
2026-04-14 08:46:30 -0300

    add Text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import './App.css'
+import "./App.css";
+import Text from "./components/Text";
 
 export default function App() {
-  return <div className='text-3xl text-green-base'>Olá Mundo!</div>
+	return (
+		<div className="text-3xl text-green-base">
+			<Text>Hello world</Text>
+		</div>
+	);
 }
